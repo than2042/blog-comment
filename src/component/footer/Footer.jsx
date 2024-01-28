af532242aac510855633516761274a4645191ef6
than2042 <than.than2042@gmail.com>
2024-01-28 15:15:16 +0000

    update link and add border on select area

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
   return (
     <div className="footerContainer">
       <p className="footerText">&copy;thanthan2024</p>
-      <Link href="https://en-gb.facebook.com/">
+      <Link href="https://en-gb.facebook.com/" replace prefetch={false}>
         <FontAwesomeIcon icon={faFacebook} />
       </Link>
-      <Link href="https://www.instagram.com/">
+      <Link href="https://www.instagram.com/" replace prefetch={false}>
         <FontAwesomeIcon icon={faInstagram} />
       </Link>
     </div>
